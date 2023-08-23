ccd8cffe3f55807ebce81cf77b4db785194ca6c4
Sheelam Aravind <102232179+AravindSheelam@users.noreply.github.com>
2023-08-23 20:47:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-window.promises = [];
+function createRandomPromise() {
+  const randomTime = Math.floor(Math.random() * 5000) + 1000; // Random time between 1 and 5 seconds in milliseconds
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`Promise resolved after ${Math.floor(randomTime / 1000)} seconds`);
+    }, randomTime);
+  });
+}
 
-// Do not change the code above this
-// add your promises to the array `promises`
+window.promises = [
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise(),
+  createRandomPromise()
+];
+
+Promise.any(window.promises)
+  .then((result) => {
+    const outputDiv = document.getElementById("output");
+    outputDiv.innerText = result;
+  })
+  .catch((error) => console.error("Error:", error));
